d8d4801831af4930409e369ffd98b23903376824
加瓦 <853715988@qq.com>
2025-04-20 11:26:51 +0800

    集成校验框架

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.jiawa.train.common.response.CommonResp;
 import com.jiawa.train.member.req.MemberRegisterReq;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class MemberController {
         return resp;
     }
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq member){
+    public CommonResp<Long> register(@Valid MemberRegisterReq member){
         CommonResp<Long> resp = new CommonResp<>();
         resp.setContent(service.register(member));
        return resp;
