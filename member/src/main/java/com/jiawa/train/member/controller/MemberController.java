5932bc9389308db7383cda179165c59c9b2db707
加瓦 <853715988@qq.com>
2025-04-21 16:44:02 +0800

    增加短信验证码接口

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -2,20 +2,23 @@ package com.jiawa.train.member.controller;
 
 import com.jiawa.train.common.response.CommonResp;
 import com.jiawa.train.member.req.MemberRegisterReq;
+import com.jiawa.train.member.req.MemberSenDCodeReq;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/member")
 public class MemberController {
     @Resource
     private MemberService service;
     @GetMapping("/hello")
     public String hello(){
-        return "HelloWorld";
+        return "HelloWorld421414";
     }
 
     @GetMapping("/count")
@@ -29,5 +32,10 @@ public class MemberController {
         CommonResp<Long> resp = new CommonResp<>();
         resp.setContent(service.register(member));
        return resp;
+    }
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid MemberSenDCodeReq member){
+        service.sendCode(member);
+        return new CommonResp<>();
     }
 }
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -8,13 +8,17 @@ import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
 import com.jiawa.train.member.req.MemberRegisterReq;
+import com.jiawa.train.member.req.MemberSenDCodeReq;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MemberService {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
     @Resource
     private MemberMapper mapper;
     public long count(){
@@ -32,5 +36,21 @@ public class MemberService {
         member1.setMobile(member.getMobile());
         mapper.insert(member1);
         return member1.getId();
+    }
+    public void sendCode(MemberSenDCodeReq req){
+        String mobile = req.getMobile();
+        MemberExample example = new MemberExample();
+        example.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = mapper.selectByExample(example);
+        if (CollUtil.isEmpty(members)){
+            LOG.info("手机号不存在，插入一条记录");
+           Member member = new Member();
+           member.setId(SnowUtil.getSnowFlakeNextId());
+           member.setMobile(mobile);
+           mapper.insert(member);
+        }
+//        String code = RandomUtil.randomString(4);
+        String code = "8888";
+        LOG.info("生成短信验证码：{}",code);
     }
 }
