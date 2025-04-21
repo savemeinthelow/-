233971683a98991dc8923024925c743e67b65273
加瓦 <853715988@qq.com>
2025-04-21 16:27:10 +0800

    修改login页面 登录注册二合一

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -1,76 +1,105 @@
 <template>
-  <a-form
-      :model="formState"
-      name="normal_login"
-      class="login-form"
-      @finish="onFinish"
-      @finishFailed="onFinishFailed"
-  >
-    <a-form-item
-        label="Username"
-        name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
-    >
-      <a-input v-model:value="formState.username">
-        <template #prefix>
-          <UserOutlined class="site-form-item-icon" />
-        </template>
-      </a-input>
-    </a-form-item>
+  <a-row class="login">
+    <a-col :span="8" :offset="8" class="login-main">
+      <h1 style="text-align: center"><rocket-two-tone />&nbsp;12306售票系统</h1>
+      <a-form
+          :model="loginForm"
+          name="normal_login"
+          autocomplete="off"
+      >
+        <a-form-item
+            label=""
+            name="mobile"
+            :rules="[{ required: true, message: '请输入手机号!' }]"
+        >
+          <a-input v-model:value="loginForm.mobile" placeholder="手机号"/>
+        </a-form-item>
 
-    <a-form-item
-        label="Password"
-        name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]"
-    >
-      <a-input-password v-model:value="formState.password">
-        <template #prefix>
-          <LockOutlined class="site-form-item-icon" />
-        </template>
-      </a-input-password>
-    </a-form-item>
+        <a-form-item
+            label=""
+            name="code"
+            :rules="[{ required: true, message: '请输入验证码!' }]"
+        >
+          <a-input v-model:value="loginForm.code">
+            <template #addonAfter>
+              <a @click="sendCode">获取验证码</a>
+            </template>
+          </a-input>
+          <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
+        </a-form-item>
 
-    <a-form-item>
-      <a-form-item name="remember" no-style>
-        <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-      </a-form-item>
-      <a class="login-form-forgot" href="">Forgot password</a>
-    </a-form-item>
+        <a-form-item>
+          <a-button type="primary" block @click="login">登录</a-button>
+        </a-form-item>
 
-    <a-form-item>
-      <a-button :disabled="disabled" type="primary" html-type="submit" class="login-form-button">
-        Log in
-      </a-button>
-      Or
-      <a href="">register now!</a>
-    </a-form-item>
-  </a-form>
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
 <script setup>
-import { reactive, computed } from 'vue';
-const formState = reactive({
-  username: '',
-  password: '',
-  remember: true,
-});
-const onFinish = values => {
-  console.log('Success:', values);
-};
-const onFinishFailed = errorInfo => {
-  console.log('Failed:', errorInfo);
-};
-const disabled = computed(() => {
-  return !(formState.username && formState.password);
+import { defineComponent, reactive } from 'vue';
+import axios from 'axios';
+import { notification } from 'ant-design-vue';
+import { useRouter } from 'vue-router'
+import store from "@/store";
+
+export default defineComponent({
+  name: "login-view",
+  setup() {
+    const router = useRouter();
+
+    const loginForm = reactive({
+      mobile: '13000000000',
+      code: '',
+    });
+
+    const sendCode = () => {
+      axios.post("/member/member/send-code", {
+        mobile: loginForm.mobile
+      }).then(response => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+    };
+
+    const login = () => {
+      axios.post("/member/member/login", loginForm).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '登录成功！' });
+          // 登录成功，跳到控台主页
+          router.push("/welcome");
+          store.commit("setMember", data.content);
+        } else {
+          notification.error({ description: data.message });
+        }
+      })
+    };
+
+    return {
+      loginForm,
+      sendCode,
+      login
+    };
+  },
 });
 </script>
-<style scoped>
-#components-form-demo-normal-login .login-form {
-  max-width: 300px;
+
+<style>
+.login-main h1 {
+  font-size: 25px;
+  font-weight: bold;
 }
-#components-form-demo-normal-login .login-form-forgot {
-  float: right;
-}
-#components-form-demo-normal-login .login-form-button {
-  width: 100%;
+.login-main {
+  margin-top: 100px;
+  padding: 30px 30px 20px;
+  border: 2px solid grey;
+  border-radius: 10px;
+  background-color: #fcfcfc;
 }
 </style>
