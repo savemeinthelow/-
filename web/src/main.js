8df603d8f51854497b9975626602c96b8e758e4f
加瓦 <853715988@qq.com>
2025-04-21 15:52:07 +0800

    集成antdesignvue及其图标

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
+import * as Icons from '@ant-design/icons-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(Antd).use(router).mount('#app')
+
+const icons = Icons;
+for (const icon in icons){
+    app.component(icon,icons[icon])
+}
+
