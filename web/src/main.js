4903a52fa1c910802b27c0992bcf72e653b3a1fe
加瓦 <853715988@qq.com>
2025-04-24 00:52:39 +0800

    为axios增加登录拦截，请求带上token

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue'
+import Antd, {notification} from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import * as Icons from '@ant-design/icons-vue'
 import axios from 'axios';
@@ -17,6 +17,10 @@ for (const icon in icons){
 
 axios.interceptors.request.use(function (config){
     console.log('请求参数',config);
+    const _token = store.state.member.token
+    if (_token){
+        config.headers.token = _token;
+    }
     return config;
 },error => {
     return Promise.reject(error)
@@ -26,6 +30,13 @@ axios.interceptors.response.use(function (response){
     return response;
 },error => {
     console.log('返回错误',error)
+    const response = error.response;
+    const status = response.status;
+    if(status===401){
+        store.commit("setMember",{})
+        notification.error({ description:'未登录或登录超时'})
+        router.push("/login")
+    }
     return Promise.reject(error);
 });
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
