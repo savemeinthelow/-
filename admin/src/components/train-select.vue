c087801a8e8ce007b7b4556fabf9432d4ac3cb7e
加瓦 <853715988@qq.com>
2025-05-02 11:11:03 +0800

    提取组件selectView

---
+++ admin/src/components/train-select.vue
@@ -0,0 +1,56 @@
+<template>
+  <a-select v-model:value="trainCode" show-search
+            :filterOption="filterTrainCodeOption">
+    <a-select-option v-for="item in trains" :key="item.code" :value="item.code"
+                     :label="item.code +item.start + item.end">
+      {{ item.code }} | {{ item.start }}~{{ item.end }}
+    </a-select-option>
+  </a-select>
+</template>
+<script>
+import {defineComponent, onMounted, ref, watch} from "vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
+
+export default defineComponent({
+  name: "train-select-view",
+  props: ["modelValue"],
+  emits: ['update:modelValue', 'change'],
+  setup(props,{emit}) {
+    const trainCode = ref()
+    const trains = ref([])
+    watch(()=> props.modelValue,()=>{
+      trainCode.value = props.modelValue
+    },{immediate:true});
+    const queryTrainCode = () => {
+      axios.get("/business/admin/train/query-all").then((response) => {
+        let data = response.data;
+        if (data.success) {
+          trains.value = data.content;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+    const filterTrainCodeOption = (input, option) => {
+      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+    };
+    onMounted(()=>{
+      queryTrainCode()
+    })
+    const onchange= (value)=>{
+      emit('update:modelValue',value);
+      let train = trains.value.filter(item=>item.code===value)[0];
+      if(Tool.isEmpty(train)){
+        train = {}
+      }
+      emit('change',train)
+    }
+    return {
+      filterTrainCodeOption,
+      trains,
+      onchange
+    }
+  }
+})
+</script>
