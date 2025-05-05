2cca55965a9efdb7beb269b1d956b55ef51c49d2
加瓦 <853715988@qq.com>
2025-05-06 01:48:24 +0800

    job模块完成

--- admin/src/components/the-sider.vue
+++ admin/src/components/the-sider.vue
@@ -7,14 +7,52 @@
         :style="{ height: '100%', borderRight: 0 }"
     >
 
-        <a-menu-item key="/welcome"><router-link to="/welcome"><coffee-outlined /> &nbsp;欢迎</router-link></a-menu-item>
-        <a-menu-item key="/about"><router-link to="/about"><coffee-outlined /> &nbsp;关于</router-link></a-menu-item>
-        <a-menu-item key="/station"><router-link to="/station"><coffee-outlined /> &nbsp;车站管理</router-link></a-menu-item>
-      <a-menu-item key="/train"><router-link to="/train"><coffee-outlined /> &nbsp;火车管理</router-link></a-menu-item>
-      <a-menu-item key="/train-station"><router-link to="/train-station"><coffee-outlined /> &nbsp;火车车站</router-link></a-menu-item>
-      <a-menu-item key="/train-carriage"><router-link to="/train-carriage"><coffee-outlined /> &nbsp;火车车厢</router-link></a-menu-item>
-      <a-menu-item key="/train-seat"><router-link to="/train-seat"><coffee-outlined /> &nbsp;火车座位</router-link></a-menu-item>
-
+      <a-sub-menu key="base">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            基础数据
+          </span>
+        </template>
+        <a-menu-item key="/base/station">
+          <router-link to="/base/station">
+            <MenuUnfoldOutlined /> &nbsp; 车站管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train">
+          <router-link to="/base/train">
+            <MenuUnfoldOutlined /> &nbsp; 火车管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-station">
+          <router-link to="/base/train-station">
+            <MenuUnfoldOutlined /> &nbsp; 火车车站
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-carriage">
+          <router-link to="/base/train-carriage">
+            <MenuUnfoldOutlined /> &nbsp; 火车车厢
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-seat">
+          <router-link to="/base/train-seat">
+            <MenuUnfoldOutlined /> &nbsp; 火车座位
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="batch">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            跑批管理
+          </span>
+        </template>
+        <a-menu-item key="/batch/job">
+          <router-link to="/batch/job">
+            <MenuUnfoldOutlined /> &nbsp; 任务管理
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
     </a-menu>
   </a-layout-sider>
 </template>
