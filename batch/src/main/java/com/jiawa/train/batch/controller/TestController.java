e307456c48c08eb7d8be19c9eccdf2de448464cd
加瓦 <853715988@qq.com>
2025-05-06 01:24:14 +0800

    batch模块新增后台接口

--- batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
+++ batch/src/main/java/com/jiawa/train/batch/controller/TestController.java
@@ -1,20 +1,24 @@
 package com.jiawa.train.batch.controller;
 
-import cn.hutool.core.util.RandomUtil;
+import com.jiawa.train.batch.feign.BusinessFeign;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Resource
+    BusinessFeign businessFeign;
+
     @GetMapping("/hello")
-    public String hello() throws InterruptedException {
-        int i = RandomUtil.randomInt(1, 10);
-        if (i <= 2) {
-            throw new RuntimeException("测试异常");
-        }
-        return "Hello World! Batch!";
+    public String hello() {
+        String businessHello = businessFeign.hello();
+        LOG.info(businessHello);
+        return "Hello World! Batch! " + businessHello;
     }
-
-
 }
